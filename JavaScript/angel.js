4d686e7726a1238928eadb4d6c31b6cb487d475f
angel <angelohikere@gmail.com>
2019-10-28 18:07:47 +0100

    Create angel.js

---
+++ JavaScript/angel.js
@@ -0,0 +1,17 @@
+Scenario 1
+==========
+Log on to "https://xkcd.com/"
+Check that the page is loaded correctly
+Click on the "Random" button
+Check that the image has changed
+
+
+Scenario 2
+==========
+
+Log on to "www.oui.sncf"
+Look for a train ticket from Paris to Bordeaux (Direct trip) departing the "12/01/2018" and returning the "17/01/2018" 
+Make sure in the results, you have the train from "Paris Montparnasse 1 & 2" leaving at "09h52" and arriving at "11h56", and select that departure
+And for the returning trip, select the one departing from Bordeaux at "16h04", and arriving at "18h08"
+Click on "Réserver"
+In the results, check that you have the price 98 euros for the train leaving at "09h52"
